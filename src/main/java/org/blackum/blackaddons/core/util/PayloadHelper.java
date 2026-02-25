0179ec7a175fa723e02f6cd7a414a1b09cb1bd8d
BLACKUM <Zipmaster2852@gmail.com>
2026-02-25 14:06:52 +0300

    fix: 1.21.10 compat

--- src/main/java/org/blackum/blackaddons/core/util/PayloadHelper.java
+++ src/main/java/org/blackum/blackaddons/core/util/PayloadHelper.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
-import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceLocation;
 import org.blackum.blackaddons.Blackaddons;
 
 public class PayloadHelper {
@@ -20,7 +20,7 @@ public class PayloadHelper {
             Class<?> clazz = original.getClass();
 
             List<Object> idList = channels.stream()
-                    .map(Identifier::tryParse)
+                    .map(ResourceLocation::tryParse)
                     .filter(Objects::nonNull)
                     .collect(Collectors.toList());
 
