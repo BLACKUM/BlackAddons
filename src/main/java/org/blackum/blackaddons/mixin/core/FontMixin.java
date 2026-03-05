c894ec2c48dee021aae09414f278ca4aa2907e32
BLACKUM <Zipmaster2852@gmail.com>
2026-03-05 15:46:24 +0300

    fix: compat with 1.21.10

--- src/main/java/org/blackum/blackaddons/mixin/core/FontMixin.java
+++ src/main/java/org/blackum/blackaddons/mixin/core/FontMixin.java
@@ -18,7 +18,7 @@ public class FontMixin {
         return CustomNameManager.getInstance().replaceInString(text);
     }
 
-    @ModifyVariable(method = "prepareText(Lnet/minecraft/util/FormattedCharSequence;FFIZZI)Lnet/minecraft/client/gui/Font$PreparedText;",
+    @ModifyVariable(method = "prepareText(Lnet/minecraft/util/FormattedCharSequence;FFIZI)Lnet/minecraft/client/gui/Font$PreparedText;",
             at = @At("HEAD"), argsOnly = true)
     private FormattedCharSequence onDrawSequence(FormattedCharSequence text) {
         return CustomNameManager.getInstance().replaceInSequence(text);
